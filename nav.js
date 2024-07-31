dc458eda2847fb951cab72a6aadca23c33ba1f90
Joanna Papadakaki <148077133+Joannapapad@users.noreply.github.com>
2024-07-31 16:16:19 +0300

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Toggle navigation menu
+    document.body.addEventListener('click', (event) => {
+        const navToggle = event.target.closest('.nav-toggle_header');
+        if (navToggle) {
+            const primaryNav = document.querySelector('.primary-navigation_header');
+            const visibility = primaryNav.getAttribute('data-visible') === "true";
+            primaryNav.setAttribute("data-visible", !visibility);
+            navToggle.setAttribute("aria-expanded", !visibility);
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
     // General Intersection Observer for animations
     const animateObserver = new IntersectionObserver(entries => {
         entries.forEach(entry => {
