621156154041c0aebdaa7eebac13c14adcc868e8
Joanna Papadakaki <148077133+Joannapapad@users.noreply.github.com>
2024-10-09 09:20:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,25 +78,6 @@ document.addEventListener('DOMContentLoaded', function () {
     showSlide(currentIndex);
 });
 
-// General Intersection Observer for animations
-const animateObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animate');
-            // Optionally unobserve after animation to improve performance
-            animateObserver.unobserve(entry.target);
-        }
-    });
-}, { threshold: 0.3 });
-
-// Select the elements to observe, now including .carousel
-const elementsToObserve = document.querySelectorAll('.text-left, .services_text-left, .subscribe,.about_services_title, .animated-line-container,.carousel ,.animated-line-container2, .heading, .project, .text-right, .vertical-separator, .services_vertical-separator, .vertical-separator2, .heropanel__content, .vertical-line-container, .customers, .row a'); 
-
-// Observe all selected elements
-elementsToObserve.forEach(element => animateObserver.observe(element));
-
-// Observe all selected elements
-elementsToObserve.forEach(element => animateObserver.observe(element));
 
     // Counter Animation
     const counters = [
