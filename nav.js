2d38b0d2b2330a6eb2616c3d526de618ed6192e5
Joanna Papadakaki <148077133+Joannapapad@users.noreply.github.com>
2024-10-09 10:18:16 +0300

    Update nav.js

--- nav.js
+++ nav.js
@@ -9,21 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
             navToggle.setAttribute("aria-expanded", !visibility);
         }
     });
-
 });
 
 // General Intersection Observer for animations
-const animateObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animate');
-            // Optionally unobserve after animation to improve performance
-            animateObserver.unobserve(entry.target);
-        }
-    });
-}, { threshold: 0.3 });
+if (typeof animateObserver === 'undefined') {
+    const animateObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                // Optionally unobserve after animation to improve performance
+                animateObserver.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.3 });
 
-// Select the elements to observe, now including .carousel
-const elementsToObserve = document.querySelectorAll('.text-left, .services_text-left, .subscribe,.about_services_title, .animated-line-container,.carousel, .animated-line-container3 ,.animated-line-container2, .heading, .project, .text-right, .vertical-separator, .services_vertical-separator, .vertical-separator2, .heropanel__content, .vertical-line-container, .customers, .row a'); 
-// Observe all selected elements
-elementsToObserve.forEach(element => animateObserver.observe(element));
+    // Select the elements to observe, now including .carousel
+    const elementsToObserve = document.querySelectorAll('.text-left, .services_text-left, .subscribe,.about_services_title, .animated-line-container,.carousel, .animated-line-container3 ,.animated-line-container2, .heading, .project, .text-right, .vertical-separator, .services_vertical-separator, .vertical-separator2, .heropanel__content, .vertical-line-container, .customers, .row a'); 
+    
+    // Observe all selected elements
+    elementsToObserve.forEach(element => animateObserver.observe(element));
+}
